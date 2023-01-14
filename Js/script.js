18bf5cff7638fd8dbcb1f497ad146e24f6a4fd3f
LUIS FERNANDO NERES DE MELO <100451426+Luisokl@users.noreply.github.com>
2023-01-13 23:01:11 -0300

    manipulando dom para inserir imagem

--- Js/script.js
+++ Js/script.js
@@ -11,15 +11,22 @@ function calcular(event) {
 
     imc = peso / (altura * altura);
     
-    resultado = document.getElementById('resultado');  
+    resultado = document.getElementById('resultado'); 
+    resultado2 = document.getElementById('resultado2');
+    novoElement = document.createElement('img');
+    resultado2.appendChild(novoElement);
+
 
     if(imc < 17) {
             // método toFixed() podemos indicar quantas casas decimais queremos apresentar.
-        resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br>Cuidado você está muito abaixo do peso!'
+        resultado.innerHTML = '<br>Seu resultado foi:  ' + imc.toFixed(0) + '<br>Cuidado você está muito abaixo do peso!'
+        novoElement.setAttribute('src', "/assets/images/homer3.png")
     } else if(imc > 17 && imc <= 18.49) {
         resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está abaixo do peso!'
+        novoElement.setAttribute('src', "/assets/images/homer3.png")
     } else if(imc >= 18.5 && imc < 24.99) {
         resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está no seu peso ideal!'
+        novoElement.setAttribute('src', "/assets/images/homer5.png")
     } else if(imc > 25 && imc <= 29.99) {
         resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está no acima do peso!'
     } else if(imc >= 30) {
