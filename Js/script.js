59d74fc0c923cbee86af57ce2878c9d1f2c2bed3
LUIS FERNANDO NERES DE MELO <100451426+Luisokl@users.noreply.github.com>
2023-01-15 10:22:06 -0300

    adicionado opçoes de menu

--- Js/script.js
+++ Js/script.js
@@ -19,19 +19,19 @@ function calcular(event) {
 
     if(imc < 17) {
             // método toFixed() podemos indicar quantas casas decimais queremos apresentar.
-        resultado.innerHTML = '<br>Seu resultado foi:  ' + imc.toFixed(0) + '<br>Cuidado você está muito abaixo do peso!'
+        resultado.innerHTML = '<br>Cuidado você está muito abaixo do peso!'
         novoElement.setAttribute('src', "/assets/images/homer3.png")
     } else if(imc > 17 && imc <= 18.49) {
-        resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está abaixo do peso!'
+        resultado.innerHTML = '<br> Você está abaixo do peso!'
         novoElement.setAttribute('src', "/assets/images/homer3.png")
     } else if(imc >= 18.5 && imc < 24.99) {
-        resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está no seu peso ideal!'
+        resultado.innerHTML = '<br> Você está no seu peso ideal!'
         novoElement.setAttribute('src', "/assets/images/homer5.png")
     } else if(imc > 25 && imc <= 29.99) {
-        resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está no acima do peso!'
+        resultado.innerHTML = '<br> Você está no acima do peso!'
         novoElement.setAttribute('src', "/assets/images/homer6.png")
     } else if(imc >= 30) {
-        resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Cuidado Obesidade!'
+        resultado.innerHTML = '<br> Cuidado Obesidade!'
         novoElement.setAttribute('src', "/assets/images/homer7.png")
     }
 
